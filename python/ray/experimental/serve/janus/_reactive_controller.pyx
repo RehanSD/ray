99f78e1813351eba6716f82c1d5355f8c0dd5a6a
Meadowlark Bradsher <mbradsher@guidewire.com>
2019-05-06 10:38:46 -0700

    working hello world cython with Bazel build

--- python/ray/experimental/serve/janus/_reactive_controller.pyx
+++ python/ray/experimental/serve/janus/_reactive_controller.pyx
@@ -1,15 +1,15 @@
 #!python
 # cython: embedsignature=True, binding=True
-#cdef extern from "fib.hpp":
-#   int fib(int s)
+cdef extern from "fib.hpp":
+   int fib(int s)
 
 def fib(int n):
-   #return fib(n)
-   cdef int i
-   cdef double a=0.0, b=1.0
-   for i in range(n):
-      a, b = a + b, a
-   return a
+   return fib(n)
+#   cdef int i
+#   cdef double a=0.0, b=1.0
+#   for i in range(n):
+#      a, b = a + b, a
+#   return a
 
 def start():
    return "start"
